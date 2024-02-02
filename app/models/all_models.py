524030d61120e9b0c0e40b84947a50050d4d740f
MaximShaybakov <momon_boton@mail.ru>
2024-02-02 15:31:30 +0500

    fix sqlalchemyentegrity_error

--- app/models/all_models.py
+++ app/models/all_models.py
@@ -21,8 +21,9 @@ class BaseUserModel(db.Model):
 
 @enum.unique
 class UserStatus(enum.Enum):
-    user = 'user'
-    admin = 'admin'
+    is_root = 'root'
+    is_user = 'user'
+    is_admin = 'admin'
 
 
 class User(BaseUserModel):
@@ -30,8 +31,6 @@ class User(BaseUserModel):
 
     password: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
-    profile: Mapped["UserProfile"] = relationship(back_populates="user",
-                                                  cascade="all, delete-orphan")
     
     def __repr__(self):
         return f'User "{self.name}"'
@@ -50,32 +49,33 @@ class UserProfile(BaseUserModel):
     last_name: Mapped[str | None] = mapped_column(String(64))
     locate: Mapped[str | None] = mapped_column(String(256))
     phone_number: Mapped[str | None] = mapped_column(String(12), unique=True)
+    age: Mapped[int | None]
+    birthdate: Mapped[datetime | None]
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
-    user: Mapped["User"] = relationship(back_populates="profile")
     
     
 class UserRequisites():
     pass
 
 
-@enum.unique
-class OrderStatus(enum.Enum):
-    AWAITING_PAYMENT = 'Awaiting payment'
-    PAID = 'Paid'
-    ANNULLED = 'Annulled'
+# @enum.unique
+# class OrderStatus(enum.Enum):
+#     AWAITING_PAYMENT = 'Awaiting payment'
+#     PAID = 'Paid'
+#     ANNULLED = 'Annulled'
 
 
-def get_user_limit(id):
-    limit = None
-    # here the user's card limit is requested
-    return limit
+# def get_user_limit(id):
+#     limit = None
+#     # here the user's card limit is requested
+#     return limit
     
 
-class Order(db.Model):
-    __tablename__ = 'order'
+# class Order(db.Model):
+#     __tablename__ = 'order'
 
-    id: Mapped[intpk]
-    limit: Mapped[Decimal] = mapped_column(get_user_limit(User.id))
-    requisites: Mapped['User'] = mapped_column(ForeignKey('users.id'))
-    user_id: Mapped[int] = relationship(back_populates='order')
-    status: Mapped['OrderStatus'] = mapped_column(nullable=False, default=OrderStatus.AWAITING_PAYMENT,)
+#     id: Mapped[intpk]
+#     user_id: Mapped[int] = relationship(back_populates='order')
+#     limit: Mapped[Decimal] = mapped_column(get_user_limit(user_id))
+#     requisites: Mapped['User'] = mapped_column(ForeignKey('users.id'))
+#     status: Mapped[OrderStatus] = mapped_column(nullable=False, default=OrderStatus.AWAITING_PAYMENT)
