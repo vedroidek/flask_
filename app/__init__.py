9d5c7cbc0ddf6b4a5e35843f1a7abcb958c52c56
MaximShaybakov <momon_boton@mail.ru>
2024-02-05 22:59:09 +0500

    .

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app(test_config=None):
     )
     
     db.init_app(app=app)
-        
+     
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
