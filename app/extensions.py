ac44f219713c3a6c70d764cced329bddc9b036a3
MaximShaybakov <momon_boton@mail.ru>
2024-02-22 08:09:50 +0500

    The transaction isolation level is set to "READ COMMITTED".

--- app/extensions.py
+++ app/extensions.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DSN = 'postgresql+psycopg2://max:4125@127.0.0.1:5431/flask_app_db'
 
-engine = create_engine(DSN, echo=True, pool_size=5, max_overflow=10)
+engine = create_engine(DSN, pool_size=5, max_overflow=10, isolation_level="READ COMMITTED")
 
 Base = declarative_base()
 
