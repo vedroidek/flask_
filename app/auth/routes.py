e8f3de87ac2e3b380f7ad63318b4bc5f979ab291
MaximShaybakov <momon_boton@mail.ru>
2024-02-28 09:44:18 +0500

    Change. Static files and views for user registration have been moved to bp:'/auth'

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,45 +1,66 @@
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask import request, make_response, render_template, flash, redirect, url_for, session
-from sqlalchemy import select
+from sqlalchemy import select, insert
+from sqlalchemy.exc import IntegrityError
+from app.redis_cli import redis_client as r
 from app.forms import LoginForm
-from app import login_manager
 from app.auth import bp
 from app.extensions import Session
 from app.models.all_models import User
 
 
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        error = None
+        
+        password=generate_password_hash(password, method='pbkdf2', salt_length=16)
+
+        if not all([username, password, email]):
+            flash('All fields are required.')
+            error = 'All fields are required.'
+        
+        if error is None:
+            try:
+                user = (insert(User).values(name=username, password=password, email=email))
+                with Session.begin() as conn:
+                    conn.execute(user)
+                    conn.commit()
+            except IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for('auth.index'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
+    """ View for user login. """
     form = LoginForm()
+    username: str = request.form.get('name')
+    password: str = request.form.get('password')
     if form.validate_on_submit():
-        username: str = request.form.get('name')
-        password: str = request.form.get('password')
-        if all([username, password]):
-            with Session() as conn:
-                user = conn.scalar(select(User).filter(User.name == username))
-                
-            password = check_password_hash(user.password, password)
+        with Session() as conn:
+            user: str = conn.scalar(select(User).filter(User.name == username))
             
-            response = make_response(redirect(url_for('home.index')))
-            
-            response.set_cookie('logged', 'yes', max_age=60*60*24)
-            return response
+        password = check_password_hash(user.password, password)
         
-        else:
-            flash('Name and/or password is invalid.', category='error')
-            render_template('auth/login.html')
+        r.set(f'is_logged:{user.name}', 'yes', ex=3600)
+        return redirect(url_for('home.index'))
+        
+    else:
+        flash('Name and/or password is invalid.', category='error')
+        render_template('auth/login.html')
     return render_template('auth/login.html', form=form)
-    # if request.cookies.get('logged') == 'yes':
-    #     return redirect('index')
-    
-    # if request.method == 'POST':
-    #     
-    
-    # return render_template('auth/login.html')
 
 
 @bp.route('/logout', methods=['GET'])
 def logout():
-    resp = make_response(redirect(url_for('home.index')))
-    resp.set_cookie('logged', 'no', max_age=0)
-    return resp
+    r.set(name=f'is_logged:{User.id}', value='no')
+    return redirect(url_for('home.index'))
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,42 +1,7 @@
-from flask import render_template, request, redirect, url_for, flash
-from sqlalchemy import insert
-from sqlalchemy.exc import IntegrityError
-from werkzeug.security import generate_password_hash
+from flask import render_template
 from app.main import bp
-from app.extensions import engine, Session
-from app.models.all_models import User
 
 
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
-
-
-@bp.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['name']
-        password = request.form['password']
-        email = request.form['email']
-        error = None
-        
-        password=generate_password_hash(password, method='pbkdf2', salt_length=16)
-
-        if not all([username, password, email]):
-            flash('All fields are required.')
-            error = 'All fields are required.'
-        
-        if error is None:
-            try:
-                user = (insert(User).values(name=username, password=password, email=email))
-                with Session.begin() as conn:
-                    conn.execute(user)
-                    conn.commit()
-            except IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for('home.index'))
-
-        flash(error)
-
-    return render_template('register.html')
